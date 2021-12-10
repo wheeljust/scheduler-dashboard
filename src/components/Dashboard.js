1793b563ae7378e55840453d48f89d138b349974
Justin Wheeler <justinwwheeler@gmail.com>
2021-12-09 22:22:49 -0700

    add selectPanel function to use for the onClick event handler

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,26 +31,38 @@ const data = [
 
 export default class Dashboard extends Component {
   state = {
-    loading: false
-  };
+    loading: false,
+    focused: null
+  }
+
+  selectPanel(id) {
+    this.setState({
+      focused: id
+    });
+  }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
-    const panels = data.map(panel => {
-      return (
-        <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
-        />
-      );
-    })
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => {
+        return (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={this.selectPanel}
+          />
+        );
+      });
 
     if (this.state.loading) {
       return <Loading />;
-    }
+    };
 
     return (
       <main className={dashboardClasses}>
