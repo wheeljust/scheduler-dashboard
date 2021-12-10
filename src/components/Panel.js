e8004b6aad7729b844dedc5512abab584894e612
Justin Wheeler <justinwwheeler@gmail.com>
2021-12-10 14:21:24 -0700

    update the onSelect handler to be a reference

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 export default class Panel extends Component {
   render() {
-    const { id, label, value, onSelect } = this.props;
+    const { label, value, onSelect } = this.props;
 
     return (
       <section
         className="dashboard__panel"
-        onClick={() => onSelect(id)}
+        onClick={onSelect}
       >
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
