d0b22aaf02291d1249167ed76dba74d78eb91578
Justin Wheeler <justinwwheeler@gmail.com>
2021-12-10 14:35:18 -0700

    add axios call to scheduler-api to replace mock data from testing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,38 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import classnames from "classnames";
 
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
+
 import Loading from "./Loading";
 import Panel from "./Panel";
-
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
 export default class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   }
 
   componentDidMount() {
@@ -41,6 +51,19 @@ export default class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -68,7 +91,7 @@ export default class Dashboard extends Component {
             key={panel.id}
             id={panel.id}
             label={panel.label}
-            value={panel.value}
+            value={panel.getValue(this.state)}
             onSelect={() => this.selectPanel(panel.id)}
           />
         );
